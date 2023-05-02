d221dcb21316a35fdd7b1979675d6d752c33b8c8
yasir-cloud9 <yasir1007@gmail.com>
2023-05-02 21:07:43 +0100

    updated POM and Test

---
+++ src/test/java/com/dailycodebuffer/ProductService/service/ProductServiceImplTest.java
@@ -0,0 +1,19 @@
+package com.dailycodebuffer.ProductService.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class ProductServiceImplTest {
+
+    @Test
+    public void addProduct() {
+    }
+
+    @Test
+    public void getProductById() {
+    }
+
+    @Test
+    public void reduceQuantity() {
+    }
+}
